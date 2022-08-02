6acfec96275029f9c731b118bcec534e5aa007b2
DaTao <DaTao@qq.com>
2022-08-02 16:11:12 +0800

    datao commit v1

---
+++ module-java/src/main/java/com/datao/maven/Hello.java
@@ -0,0 +1,11 @@
+package com.datao.maven;
+
+public class Hello {
+
+    public void showMessage(){
+        System.out.println("Hello Datao");
+    }
+    public static void main(String[] args){
+        System.out.println("woshi shuaige");
+    }
+}
---
+++ module-java/src/test/java/com/datao/maven/HelloTest.java
@@ -0,0 +1,11 @@
+package com.datao.maven;
+
+import org.junit.Test;
+
+public class HelloTest {
+    @Test
+    public void testHello(){
+        Hello hello = new Hello();
+        hello.showMessage();
+    }
+}
